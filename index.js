d3a44b3cd8e1c22ac5141efa8ef811c44ca35cc7
Redi Ahmad <redi.ngoding@gmail.com>
2023-02-01 00:16:52 +0700

    feat: ability to detect the language
    Using ranc and langs packages, the app can now detect the language of text given via CLI argument. But, I need to refactor the code, it is so hard to read

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const process = require('process');
-import { franc, francAll } from 'franc';
+
+import { franc } from 'franc';
+const langs = require('langs');
 
 const args = process.argv.slice(2);
 console.table(args);
+
+/**
+ * detectLanguage will detect the language of given argument.
+ * @param {string} sentence - A word or sentence to test.
+ * @returns {string} - Language code consists of three character.
+ */
+function detectLanguage(sentence) {
+  return franc(sentence);
+}
+
+console.log(langs.where('2T', detectLanguage(args[0])).name);
