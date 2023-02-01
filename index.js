20922dd79e53560939b1b6351408dbeb2852fe7c
Allday <aldaemly2825@gmail.com>
2023-02-01 23:09:40 +0700

    Add undetermined language handler

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ console.table(args);
 /**
  * detectLanguage will detect the language of given argument.
  * @param {string} sentence - A word or sentence to test.
- * @returns {string} - Language code consists of three character.
+ * @returns {string} - Language code consists of three character. May return 'und' which means 'undetermined', so handle it correctly.
  */
 function detectLanguage(sentence) {
-  return franc(sentence);
+  return franc(sentence, { minLength: 3 });
 }
 
-console.log(langs.where('2T', detectLanguage(args[0])).name);
+
+if (detectLanguage(args[0] === 'und')) console.error(`Sorry, we could not detect the language :(`)
+else console.log(langs.where('2', detectLanguage(args[0])).name);
